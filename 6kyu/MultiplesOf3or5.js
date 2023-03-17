8010fd7e86bbb17feef04fa4808216b77cdf8dcc
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2023-03-16 23:39:09 -0400

    implement solution for numbers 3 and below

--- 6kyu/MultiplesOf3or5.js
+++ 6kyu/MultiplesOf3or5.js
@@ -1,18 +1,23 @@
 function solution(number){
   // return 0 for negative number
-  if (number < 0) {
+  if (number <= 0) {
     return 0;
   }
 
-  const multiplesArray = [];
   for (let i = 1; i < number; i++) {
+    const multiplesArray = [];
     // find multiples of 5 & 3
-    if (i % 5 === 0 && i % 3 ===0) {
+    if (i % 5 === 0 && i % 3 === 0) {
+      multiplesArray.push(i);
+    } else if (i % 5 === 0) {
+      // find multiples of 5
+      multiplesArray.push(i);
+    } else if (i % 3 === 0) {
+      // find multiples of 3
       multiplesArray.push(i);
     }
-    // find multiples of 5
-
-    // find multiples of 3
-    // return sum of all multiples
+    console.log(multiplesArray);
   }
+  // return sum of all multiples
 }
+console.log('solution: ',solution(0));
