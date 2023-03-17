d968042cf49cbbfe20f45590d400712920b26709
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2023-03-16 23:30:02 -0400

    implement return zero for negative number

--- 6kyu/MultiplesOf3or5.js
+++ 6kyu/MultiplesOf3or5.js
@@ -1,5 +1,8 @@
 function solution(number){
   // return 0 for negative number
+  if (number < 0) {
+    return 0;
+  }
   // find multiples of 5 & 3
   // find multiples of 5
   // find multiples of 3
