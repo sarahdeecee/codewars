9fdc6da188e5a88efeda4861243dbf46555a0f45
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2023-03-25 00:40:17 -0400

    solve rgb function

--- 5kyu/RGBtoHexConversion.js
+++ 5kyu/RGBtoHexConversion.js
@@ -1,3 +1,32 @@
 function rgb(r, g, b){
-  // complete this function  
+  const convert = num => {
+    // Edge cases
+    if (num >= 255) {
+      return 'FF'
+    } else if (num <= 0) {
+      return '00';
+    }
+    
+    // Not edge case
+    const quotient =  Math.floor(num / 16);
+    const remainder = num % 16;
+    
+    const charcodeZero = 48; // 0
+    const charcodeLetter = 48 + 7; // A
+
+    const convertDigit = digit => {
+      if (digit === 0) {
+        return '0';
+      } else if (digit >= 15) {
+        return 'F';
+      } else if (digit > 9) {
+        return String.fromCharCode(charcodeLetter + digit);
+      } else {
+        return String.fromCharCode(charcodeZero + digit);
+      }
+    }
+    return convertDigit(quotient) + convertDigit(remainder);
+  }
+  
+  return convert(r) + convert(g) + convert(b);
 }
