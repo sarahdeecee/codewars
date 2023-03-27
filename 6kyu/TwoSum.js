f7dce9a123db2786398ad7327bda4ba1e4bb8053
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2023-03-27 15:04:03 -0400

    add solution for Two Sum

--- 6kyu/TwoSum.js
+++ 6kyu/TwoSum.js
@@ -1,3 +1,14 @@
 function twoSum(numbers, target) {
-  // ...
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = 1; j < numbers.length; j++) {
+      if (i !== j) {
+        if (numbers[i] + numbers[j] === target) {
+          return [i, j];
+        }
+      }
+    }
+  }
+  return undefined;
 }
+
+console.log(twoSum([1, 2, 3], 4));
