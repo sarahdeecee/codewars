905e273253cf32cffcfbe610e08f4b051eb6f8b6
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2023-03-27 14:28:57 -0400

    solve create phone number

--- 6kyu/CreatePhoneNumber.js
+++ 6kyu/CreatePhoneNumber.js
@@ -1,3 +1,14 @@
 function createPhoneNumber(numbers){
-  
+  const numbersWithSpacers = ['('];
+  for (let num of numbers) {
+    if (numbersWithSpacers.length === 4) {
+      numbersWithSpacers.push(') ',num.toString());
+    } else if (numbersWithSpacers.length === 8) {
+      numbersWithSpacers.push('-',num.toString())
+    } else {
+      numbersWithSpacers.push(num.toString());
+    }
+  }
+  return numbersWithSpacers.join('');
 }
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
